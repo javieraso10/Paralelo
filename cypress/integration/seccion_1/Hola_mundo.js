f1e890f0c8a818f6ffb2521f7e388fe0bae6bf72
jalvareza <javi_vierin@hotmail.es>
2022-03-25 09:47:37 +0100

    prueba

--- cypress/integration/seccion_1/Hola_mundo.js
+++ cypress/integration/seccion_1/Hola_mundo.js
@@ -8,7 +8,7 @@ describe("Bienvenido al curso seccion1",()=>{
     it('segundo test', () => {
         cy.visit("https://demoqa.com/text-box")
         cy.get("#userName").type("HUERTINAAAAA")
-        cy.wait(5000)
+        cy.wait(4000)
 
     });
 })//cierre
